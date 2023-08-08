a4639521d438331fc53ae9392c61ed5505a002f9
ValterMonteiro <valtinms@gmail.com>
2023-08-08 14:40:47 -0400

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
+    { title: 'Home', url: '/home', icon: 'home' },
+    { title: 'Button', url: '/button', icon: 'finger-print' },
+    { title: 'Lista', url: '/lista', icon: 'list' },
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
