dc97e0383904821563840500ecdd514448cadcf4
ValterMonteiro <valtinms@gmail.com>
2023-08-15 00:30:01 -0400

    atualizando pagina de lista

--- src/app/pagina/lista/lista.page.ts
+++ src/app/pagina/lista/lista.page.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPage implements OnInit {
 
+  atores = [
+    {
+      nome: "Daenerys Targaryen",
+      avatar: "/assets/imgDaenerys.png",
+      estrelas: "25"
+    },
+    {
+      nome: "Jon Snow",
+      avatar: "/assets/imgJonSnow.png",
+      estrelas: "21"
+    }
+  ]
+
+/* inserindo cards */
+nome = "Daenerys Targaryen";
+avatar = "/assets/imgDaenerys.png";
+
   constructor() { }
 
   ngOnInit() {
